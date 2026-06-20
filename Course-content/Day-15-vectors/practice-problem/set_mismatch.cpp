40b4a318d27ce3cf7b7cae3da8bfcb0f84cdb0fc
Ashmit Saxena <Saxenaashmit2005@gmail.com>
2026-06-20 23:39:12 +0530

    day 15 revision and practice problem

---
+++ Course-content/Day-15-vectors/practice-problem/set_mismatch.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> findErrorNums(vector<int> nums)
+{
+    vector<int> ans;
+    int n = nums.size();
+
+    vector<bool> isPresent(n + 1, false);
+
+    // Find duplicate number
+    for (int i = 0; i < n; i++)
+    {
+        if (isPresent[nums[i]])
+        {
+            ans.push_back(nums[i]); // duplicate
+        }
+        isPresent[nums[i]] = true;
+    }
+
+    // Find missing number
+    for (int i = 1; i <= n; i++)
+    {
+        if (!isPresent[i])
+        {
+            ans.push_back(i); // missing
+            break;
+        }
+    }
+
+    return ans;
+}
+
+int main()
+{
+    vector<int> nums = {1, 2, 2, 4};
+
+    vector<int> ans = findErrorNums(nums);
+
+    cout << "[" << ans[0] << ", " << ans[1] << "]" << endl;
+
+    return 0;
+}
