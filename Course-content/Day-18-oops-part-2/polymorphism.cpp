972f3f6491244a9b3a3ec36b9e7e86c913866193
Ashmit Saxena <Saxenaashmit2005@gmail.com>
2026-06-28 20:23:27 +0530

    Added day 18 oops part 2

---
+++ Course-content/Day-18-oops-part-2/polymorphism.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // polymorphism is the ability of objects to take on different forms or behave in ways depending on context in which they are used
+
+    // types   
+    // compile type polymorphism 
+    // run time polymorphism
+    
+    return 0;
+}
