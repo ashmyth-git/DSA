cb14ba10e063048ac2f7126792f182657b33d465
Ashmit Saxena <Saxenaashmit2005@gmail.com>
2026-07-02 23:36:22 +0530

    update binary string problem

--- Course-content/Day-20-recursion-part-2/binary_strings_problem.cpp
+++ Course-content/Day-20-recursion-part-2/binary_strings_problem.cpp
@@ -1,22 +1,28 @@
+// print binary string
 #include <iostream>
 using namespace std;
 
-void binaryString(int n, int lastPlace, string ans) {
-    if (n == 0) {
+void binaryString(int n, int lastPlace, string ans)
+{
+    if (n == 0)
+    {
         cout << ans << endl;
         return;
     }
-
-    // Always place 0
-    binaryString(n - 1, 0, ans + '0');
-
-    // Place 1 only if previous bit was not 1
-    if (lastPlace != 1) {
-        binaryString(n - 1, 1, ans + '1');
+    
+    if (lastPlace != 1)
+    {
+        binaryString(n - 1, 0, ans + '0');
+        binaryString(n-1, 1, ans + '1');
     }
+    else{
+        binaryString(n - 1, 0, ans + '0');
+    }
+    
 }
 
-int main() {
+int main()
+{
     binaryString(3, 0, "");
     return 0;
 }
