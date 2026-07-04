9807536273211562189f630945ca4df18c32c096
Ashmit Saxena <Saxenaashmit2005@gmail.com>
2026-07-04 11:16:23 +0530

    Added day 22 and 23 of time and space complexity

---
+++ Course-content/Day-22-time-and-space-complexity-part-1/space_complexity.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // amount of extra space occupied by an algorithm as an funciton of input size(n)
+    return 0;
+}
---
+++ Course-content/Day-22-time-and-space-complexity-part-1/time_complexity.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // the amount of time taken up by an algorithm as a function of input size(n)
+
+    // types of time complexity
+    // 1. constant time complexity - O(1)
+    // 2. O(n2) quadratic TC
+    // 3. O(n) linear TC
+    // 4. O(logn) logarithmic TC
+    // 4. O(2^n) exponential TC
+
+    // if time complexity is given in the function like quadratic in n or any other ... then the time complexity is the largest one
+
+    // n^2 + n + 1 = timecomplexity(n^2)ans..
+
+    // other notations of time complexity
+    // 1. big omega - best time complexity
+    // 2. big theta - average time complexity
+    return 0;
+}
