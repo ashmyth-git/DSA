edc3b973c7d7add666d776310d4b2c29c61f646e
Ashmit Saxena <Saxenaashmit2005@gmail.com>
2026-07-05 17:37:12 +0530

    Added day 24 backtracking

---
+++ Course-content/Day-24-backtracking/introduction_and_type_of_backtracking.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // types of backtracking
+    // 1. decision : yes or no
+    // 2. optimization: best one
+    // 3. enumerization: count 
+    
+    return 0;
+}
