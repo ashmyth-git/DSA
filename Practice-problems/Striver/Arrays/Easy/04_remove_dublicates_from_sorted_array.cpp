f0e21759dcd226c18178b26e03623c82512b434b
Ashmit Saxena <Saxenaashmit2005@gmail.com>
2026-07-09 23:57:42 +0530

    Update remove_dublicate_element_in_sorted_array problem

--- Practice-problems/Striver/Arrays/Easy/04_remove_dublicates_from_sorted_array.cpp
+++ Practice-problems/Striver/Arrays/Easy/04_remove_dublicates_from_sorted_array.cpp
@@ -2,10 +2,11 @@
 #include <vector>
 using namespace std;
 
-int removeDuplicates(vector<int> &nums, int n)
+int removeDuplicates(vector<int> &nums)
 {
-    if (nums.empty())
+    if (nums.size() == 0){
         return 0;
+    }
 
     int k = 1;
 
@@ -17,13 +18,29 @@ int removeDuplicates(vector<int> &nums, int n)
             k++;
         }
     }
+
+    // IMP--  by printing we understand the problem more correctly which is already given in the problem statement 
+    // the thing is that it reinitialise the array with the unique elements only like 
+    /*
+        num[0] = 0 already there
+        num[1] = 1 when i = 2
+        num[2] = 2 when i = 5
+        num[3] = 3 when i = 7
+        num[4] = 4 when i = 9
+    */
+   // the output is only valid till the value of k that is 5 in that case afterwards the values are useless or we can say not unique or extra
+    for (int i = 0; i < nums.size(); i++)
+    {
+        cout<<nums[i]<<" ";
+    }
+    cout<<endl;
+
     return k;
 }
 
 int main()
 {
     vector<int> nums = {0,0,1,1,1,2,2,3,3,4};
-    int n = nums.size();
-    cout<<removeDuplicates(nums, n);
+    cout<<removeDuplicates(nums);
     return 0;
 }
