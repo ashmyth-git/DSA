f1298fc412cd0e8d372a64f1f0cfe2e7713ef529
Ashmit Saxena <Saxenaashmit2005@gmail.com>
2026-07-15 15:07:20 +0530

    Add longest_subarray_with_given_sum_k problem

---
+++ Practice-problems/Striver/Arrays/Easy/13_longest_subarray_with_given_sum_K_positive_brute_force.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int longestSubarray(vector<int> &nums, int k)
+{
+    int n = nums.size();
+    int maxLength = 0;
+
+    for (int startIndex = 0; startIndex < n; startIndex++)
+    {
+        for (int endIndex = startIndex; endIndex < n; endIndex++)
+        {
+            int currentSum = 0;
+            for (int i = startIndex; i <= endIndex; i++)
+            {
+                currentSum += nums[i];
+            }
+            if (currentSum == k)
+            {
+                maxLength = max(maxLength, endIndex - startIndex + 1);
+            }
+        }
+    }
+    return maxLength;
+}
+
+int main()
+{
+    vector<int> nums = {-1, 1, 1};
+    int k = 1;
+    cout<<longestSubarray(nums, k);
+    return 0;
+}
---
+++ Practice-problems/Striver/Arrays/Easy/13_longest_subarray_with_given_sum_K_positive_optimal.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int longestSubarray(vector<int> &nums, int k)
+{
+    int n = nums.size();
+    int maxLen = 0;
+    int left = 0, right = 0;
+    int sum = nums[0];
+
+    while (right < n)
+    {
+
+        while (left <= right && sum > k)
+        {
+            sum -= nums[left];
+            left++;
+        }
+        if (sum == k)
+        {
+            maxLen = max(maxLen, right - left + 1);
+        }
+        right++;
+        if (right < n)
+        {
+            sum += nums[right];
+        }
+    }
+    return maxLen;
+}
+
+int main()
+{
+    vector<int> nums = {10, 5, 2, 7, 1, 9};
+    int k = 15;
+    cout<<longestSubarray(nums, k);
+    return 0;
+}
